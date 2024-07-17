1191f2f521522bb46ba5981e2db86d6ed7775396
Luquinhas006 <lzcastro04@gmail.com>
2024-07-17 17:45:40 -0300

    Update list_custom.dart
    não é list view

--- lib/model/list_view.dart
+++ lib/model/list_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ListView extends StatelessElement {
-  ListView(super.widget);
+class ListCustom extends StatelessWidget {
   List<Map<String, String>> products = [
     {
       'name': 'Aspirina',
@@ -29,4 +28,35 @@ class ListView extends StatelessElement {
       'subtitle': 'Antiparasitário'
     },
   ];
+
+  ListCustom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ListView.separated(
+      padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+        leading: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(shape: BoxShape.circle),
+            clipBehavior: Clip.antiAlias,
+            child: Image.asset(products[index]['image']!, height: 100, width: 100, fit: BoxFit.fill,),
+          ) ,
+          title: Text(products[index]['name']!, style: TextStyle(fontSize: 16, fontStyle: FontStyle.normal, color: Colors.black),),
+          subtitle: Text(products[index]['subtitle']!, style: TextStyle(fontSize: 12, color: const Color.fromARGB(255, 80, 80, 80)),),
+          trailing: Icon(Icons.add),
+          contentPadding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
+        );
+      },
+      separatorBuilder: (context, index) {//<-- SEE HERE
+        return Divider(
+          thickness: 1,
+        );
+      },
+    );
+  }
 }
